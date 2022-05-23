6ce1448590e987644808fdfae1f9bc6756b9614b
morteza-sharifi <100284021+morteza-sharifi@users.noreply.github.com>
2022-05-23 08:07:55 -0700

    created the makeCacheMatrix function.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+  #In the makeCacheMatrix function, we create and 
+# save values and functions in the parent environment
 
 makeCacheMatrix <- function(x = matrix()) {
+  #If it has a value equal to null
+  i <<- NULL
+  set <- function(y) 
+  {
+  x <<- y
+  i <<- NULL
+  }
+  
+get <- function() x
+
+setinverse <- function(inverse) i <<- inverse
+
+getinverse <- function() i
+
+list(set = set , get = get, setinverse = setinverse , getinverse = getinverse)
 
 }
-
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
